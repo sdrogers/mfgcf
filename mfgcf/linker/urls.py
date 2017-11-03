1a0192b81e6f20ef9e0b64add9bd29a2930ea6c2
= <=>
2017-11-03 20:47:36 +0000

    got a graph going

--- mfgcf/linker/urls.py
+++ mfgcf/linker/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 urlpatterns = [
 	url(r'^$',views.index,name = 'index'),
-	url(r'^example_graph/$', views.example_graph,name='example_graph')
+	url(r'^show_graph/$', views.show_graph,name='show_graph'),
+	url(r'^get_graph/$', views.get_graph,name='get_graph'),
+	url(r'^get_gcf_strains/(?P<gcf_id>\w+)$',views.get_gcf_strains,name = 'get_gcf_strains'),
+	url(r'^get_mf_strains/(?P<mf_id>\w+)$',views.get_mf_strains,name = 'get_mf_strains'),
+	url(r'^get_overlap_strains/(?P<gcf_id>\w+)/(?P<mf_id>\w+)$',views.get_overlap_strains,name = 'get_overlap_strains'),
 ]
 
--- mfgcf/linker/views.py
+++ mfgcf/linker/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from scipy.stats import hypergeom
 # Create your views here.
 import json
@@ -9,39 +9,59 @@ import numpy as np
 from linker.models import *
 
 def index(request):
-	return HttpResponse("Hello")
-
-def example_graph(request):
-	mfs = MF.objects.all()
-	gcfs = GCF.objects.all()
-
-	strains = Strain.objects.all()
-	mfs_list = []
-	for m in mfs:
-		n = len(MFStrain.objects.filter(mf = m))
-		mfs_list.append(["MF{}".format(m.name),{'nstrains': n}])
-	gcf_list = []
-	for g in gcfs:
-		n = len(GCFStrain.objects.filter(gcf = g))
-		gcf_list.append(["GCF{}".format(g.name),{'nstrains': n,'gcftype': g.gcftype}])
-
-	# context_dict = {'mfs':json.dumps(['MF{}'.format(m.name) for m in mfs])}
-	context_dict = {}
-	context_dict['mfs'] = json.dumps(mfs_list)
-	context_dict['strains'] = json.dumps([s.name for s in strains])
-	context_dict['gcfs'] = json.dumps(gcf_list)
-	edges = []
-
-	p_thresh = 0.01
-	links = MFGCFEdge.objects.filter(p__lte = p_thresh)
-	for link in links:
-	    edges.append(["MF{}".format(link.mf.name),"GCF{}".format(link.gcf.name),-np.log(link.p)])
-	        
-	# edges.append([mfstrains.strain.name,mfstrains.mf.name])
-	context_dict['edges'] = json.dumps(edges)
-
-	
+    return HttpResponse("Hello")
 
 
+def show_graph(request):    
+    return render(request,'linker/show_graph.html',{})
 
-	return render(request,'linker/example_graph.html',context_dict)
+def get_overlap_strains(request,gcf_id,mf_id):
+    gcf = GCF.objects.get(id = gcf_id)
+    mf = MF.objects.get(id = mf_id)
+    gcf_strains = set([g.strain for g in gcf.gcfstrain_set.all()])
+    mf_strains = set([g.strain for g in mf.mfstrain_set.all()])
+    overlap = gcf_strains.intersection(mf_strains)
+    straindict = {'strains': [s.name for s in overlap]}
+    return JsonResponse(straindict)
+
+def get_gcf_strains(request,gcf_id):
+    gcf = GCF.objects.get(id = gcf_id)
+    strains = [g.strain for g in gcf.gcfstrain_set.all()]
+    strainlist = [s.name for s in strains]
+    straindict = {'strains':strainlist}
+    return JsonResponse(straindict)
+
+def get_mf_strains(request,mf_id):
+    mf = MF.objects.get(id = mf_id)
+    strains = [g.strain for g in mf.mfstrain_set.all()]
+    strainlist = [s.name for s in strains]
+    straindict = {'strains':strainlist}
+    return JsonResponse(straindict)
+
+def get_graph(request):
+    import networkx as nx
+    from networkx.readwrite import json_graph
+
+    G = nx.Graph()
+    mfs = MF.objects.all()
+    gcfs = GCF.objects.all()
+
+    strains = Strain.objects.all()
+    mfs_list = []
+    for m in mfs:
+        n = len(MFStrain.objects.filter(mf = m))
+        mfs_list.append(["MF{}".format(m.name),{'nstrains': n}])
+        G.add_node("MF{}".format(m.name),nstrains = n,nodetype='mf',dbid = m.id)
+    gcf_list = []
+    for g in gcfs:
+        n = len(GCFStrain.objects.filter(gcf = g))
+        gcf_list.append(["GCF{}".format(g.name),{'nstrains': n,'gcftype': g.gcftype}])
+        G.add_node("GCF{}".format(g.name),nstrains = n,gcftype = g.gcftype,nodetype='gcf',dbid = g.id)
+
+
+    p_thresh = 0.01
+    links = MFGCFEdge.objects.filter(p__lte = p_thresh)
+    for link in links:
+        G.add_edge("MF{}".format(link.mf.name),"GCF{}".format(link.gcf.name),weight =-np.log(link.p))
+            
+    return JsonResponse(json_graph.node_link_data(G))
