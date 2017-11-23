0208a11a892cbc1828553d5b63d2eaddda06a97b
Rónán Daly <ronan.daly@glasgow.ac.uk>
2017-11-23 12:19:46 +0000

    Stop collectstatic asking for input

--- mfgcf/start_prod_server.sh
+++ mfgcf/start_prod_server.sh
@@ -3,5 +3,5 @@
 export DJANGO_SETTINGS_MODULE=mfgcf.settings_prod
 
 . ../../venv/mfgcf/bin/activate
-python manage.py collectstatic
+python manage.py collectstatic --no-input
 exec gunicorn mfgcf.wsgi -w 5 --user=mfgcf --group=mfgcf
