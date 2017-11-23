270d0b59b359d6e8482dd229c657ed6b8162da28
Rónán Daly <ronan.daly@glasgow.ac.uk>
2017-11-23 17:06:23 +0000

    Making sure environment variables are exported

--- mfgcf/start_prod_server.sh
+++ mfgcf/start_prod_server.sh
@@ -3,6 +3,8 @@
 export DJANGO_SETTINGS_MODULE=mfgcf.settings_prod
 
 . ../../venv/mfgcf/bin/activate
+set -a
 . .env
+set +a
 python manage.py collectstatic --no-input
 exec gunicorn mfgcf.wsgi -w 5 --user=mfgcf --group=mfgcf
