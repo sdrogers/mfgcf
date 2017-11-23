195cc9b76183f6d40fc62e942bdca361832556d5
Joe Wandy <joe.wandy@glasgow.ac.uk>
2017-11-23 15:52:19 +0000

    Changed database to MySQL server in production

--- mfgcf/mfgcf/settings_prod.py
+++ mfgcf/mfgcf/settings_prod.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'mfgcf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mfgcf',
+        'USER': 'mfgcf',
+        'PASSWORD': 'WdEZ4muJmX',
+        'HOST': '',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
