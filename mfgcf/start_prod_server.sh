d4939e02592e392f7d8282a1ce4acbffd946e965
Rónán Daly <ronan.daly@glasgow.ac.uk>
2017-11-23 16:55:40 +0000

    Evaluate .env when starting production server

--- mfgcf/start_prod_server.sh
+++ mfgcf/start_prod_server.sh
@@ -3,5 +3,6 @@
 export DJANGO_SETTINGS_MODULE=mfgcf.settings_prod
 
 . ../../venv/mfgcf/bin/activate
+. .env
 python manage.py collectstatic --no-input
 exec gunicorn mfgcf.wsgi -w 5 --user=mfgcf --group=mfgcf
