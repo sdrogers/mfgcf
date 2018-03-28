34eb6f027df494e4c326533fb0a171dcdff203aa
Grimur Hjorleifsson <grimurh@gmail.com>
2018-03-28 09:47:08 +0100

    Accept data source as parameter. Cleanup.

--- mfgcf/addgcfanalysis.py
+++ mfgcf/addgcfanalysis.py
@@ -13,45 +13,27 @@ from django.db import transaction
 from linker.models import *
 
 
-GCF_TYPES = ['allRiPPs',
-             'allPKSother',
-             'allOthers',
-             'allNRPS',
-             'allPKSI',
-             'allPKS-NRP_Hybrids',
-             'allSaccharides',
-             'allTerpene']
-
-GCF_TYPES2 = ['All_RiPPs',
-              'All_PKSother',
-              'All_Others',
-              'All_NRPS',
-              'All_PKSI',
-              'All_PKS-NRP_Hybrids',
-              'All_Saccharides',
-              'All_Terpene']
-
 # strain_ids.csv maps all the various ids to genbank ids
 strain_id_file = 'strain_ids.csv'
 
 
 def preprocess_genbank_name(string):
-                genbank_name = string.split('_')[0]
-                if '.' in genbank_name:
-                    genbank_name = genbank_name.split('.')[0]
-                if genbank_name == 'GCA':
-                    # hack!
-                    genbank_name = '_'.join(string.split('_')[:2])
-                    genbank_name = genbank_name.split('.')[0]
-                return genbank_name
+    genbank_name = string.split('_')[0]
+    if '.' in genbank_name:
+        genbank_name = genbank_name.split('.')[0]
+    if genbank_name == 'GCA':
+        # hack!
+        genbank_name = '_'.join(string.split('_')[:2])
+        genbank_name = genbank_name.split('.')[0]
+    return genbank_name
 
 
 def string_to_genbank(genbank_name):
     strain_name = None
     if genbank_name in strain_dict:
         strain_name = strain_dict[genbank_name]
-    else:
-        print "{} STRAIN NOT FOUND!".format(genbank_name)
+    # else:
+    #     print "{} STRAIN NOT FOUND!".format(genbank_name)
     return strain_name
 
 
@@ -60,13 +42,13 @@ def string_to_mibig(genbank_name):
     # check if it is a bgc
     try:
         mibig = MiBIG.objects.get(name=genbank_name)
-        print "\t its from MiBIG!"
+        # print "\t its from MiBIG!"
     except:
         pass
     return mibig
 
 
-def load_gcf_trio(analysis, family_file, annotations_file, strain_dict, gcf_type=None):
+def process_gcf_files(analysis, family_file, annotations_file, strain_dict, gcf_type=None, source=None):
     # This file includes the BGC info
     bgc_dict = {}
     with transaction.atomic():
@@ -98,11 +80,9 @@ def load_gcf_trio(analysis, family_file, annotations_file, strain_dict, gcf_type
                     if mibig is not None:
                         bgc.mibig = mibig
                         bgc.save()
+                    else:
+                        print "Strain {} not found!".format(strain_name)
 
-    # with open(network_file,'r') as f:
-    #   for line in f:
-    #       print line
-    #       break
     with open(family_file, 'r') as f:
         with transaction.atomic():
             reader = csv.reader(f, delimiter='\t')
@@ -123,7 +103,7 @@ def load_gcf_trio(analysis, family_file, annotations_file, strain_dict, gcf_type
                         # link gcftype and gcf
                         gcflink, link_created = GCFtoClass.objects.get_or_create(gcf=gcf, gcfclass=gcfclass)
                         if class_created:
-                            gcfclass.source = 'BIGSCAPE'
+                            gcfclass.source = source
                             gcfclass.save()
                         if link_created:
                             gcflink.save()
@@ -134,21 +114,6 @@ def load_gcf_trio(analysis, family_file, annotations_file, strain_dict, gcf_type
 
                 BGCGCF.objects.get_or_create(bgc=bgc, gcf=gcf)
     return strain_dict
-
-
-def get_files(bigscape_outout_dir):
-    # Get the trios of files from the output dir
-    file_trios = []
-    for i, t in enumerate(GCF_TYPES):
-        network_file = glob.glob(os.path.join(bigscape_outout_dir, t + '*.network'))
-        if len(network_file) == 1:
-            network_file = network_file[0]
-            sub_name = ".".join(network_file.split(os.sep)[-1].split('.')[:-1])
-            last_bit = sub_name.split('_')[-1]
-            tsv_file = os.path.join(bigscape_outout_dir, t + '_clustering_' + last_bit + '.tsv')
-            annotations_file = os.path.join(bigscape_outout_dir, 'Network_Annotations_' + GCF_TYPES2[i] + '.tsv')
-            file_trios.append((network_file, tsv_file, annotations_file))
-    return file_trios
 
 
 def get_strain_dict():
@@ -174,12 +139,14 @@ if __name__ == '__main__':
     parser.add_argument('family', help='Family .tsv file')
     parser.add_argument('annotations', help='BGC annotations file')
     parser.add_argument('-t', dest='type', help='BGC type', default=None)
+    parser.add_argument('-s', dest='source', help='Data source', default=None)
     args = parser.parse_args()
 
     analysis_name = args.name
     family_file = args.family
     annotations_file = args.annotations
     bgc_type = args.type
+    bgc_source = args.source
 
     strain_dict = get_strain_dict()
 
@@ -193,7 +160,6 @@ if __name__ == '__main__':
     # network file            - contains detailed info about the links btw. BGCs (incl. individual weights)
     # tsv clustering file     - BGC : family pairs
     # network annotation file - individual BGC annotations
-    # file_trios = get_files(bigscape_outout_dir)
 
     print "Adding BGCs (and strains) from {}".format(family_file)
-    strain_dict = load_gcf_trio(analysis, family_file, annotations_file, strain_dict, bgc_type)
+    strain_dict = process_gcf_files(analysis, family_file, annotations_file, strain_dict, bgc_type, bgc_source)
